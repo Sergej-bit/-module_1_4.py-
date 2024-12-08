553bd83331908d3d961b624ee2d693a982752f9e
Sergej-bit <khalyavkin.s@yandex.ru>
2024-12-08 11:42:11 +0300

    Add files via upload

---
+++ 'module_1_4.py
@@ -0,0 +1,8 @@
+my_string = 'Добрый день'
+print(my_string)
+print(len(my_string))
+print('Добрый день'. upper())
+print('Добрый день'. lower())
+print('Добрый день'. replace(' ', '' ))
+print(my_string[0])
+print(my_string[-1])
